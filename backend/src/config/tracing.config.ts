7987bedb7dbb41fe762a3e21ae17fddc0e15e8b4
Paulo Campos <paulo4-0@hotmail.com>
2025-04-01 23:11:07 -0300

    feat: add monitoring and logging configuration with Winston and OpenTelemetry

---
+++ backend/src/config/tracing.config.ts
@@ -0,0 +1,22 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+
+export const otelSDK = new NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    url:
+      process.env.OTEL_EXPORTER_OTLP_ENDPOINT ||
+      'http://localhost:4318/v1/traces',
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+process.on('SIGTERM', () => {
+  otelSDK
+    .shutdown()
+    .then(
+      () => console.log('SDK shut down successfully'),
+      (err) => console.log('Error shutting down SDK', err),
+    )
+    .finally(() => process.exit(0));
+});
